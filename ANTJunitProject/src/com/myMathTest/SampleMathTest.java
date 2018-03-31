8b7600d9194fbd677e1e0c1263ce26b209688e27
Vikas <vikas_210@yahoo.com>
2018-03-31 23:42:18 +1100

    With 2 Tests

---
+++ ANTJunitProject/src/com/myMathTest/SampleMathTest.java
@@ -0,0 +1,39 @@
+package com.myMathTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import junit.framework.Assert;
+
+class SampleMathTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSum() {
+		assertEquals(10+10, 20);
+	}
+	@Test
+	void testProduct() {
+		assertEquals(10*10, 20);
+	}
+}

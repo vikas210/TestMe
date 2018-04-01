1151e53661c30e9387f1c4d3cdad49c1396e0d77
Vikas <vikas_210@yahoo.com>
2018-04-01 15:41:49 +1000

    Junit 4

--- ANTJunitProject/src/com/myMathTest/SampleMathTest.java
+++ ANTJunitProject/src/com/myMathTest/SampleMathTest.java
@@ -1,39 +1,26 @@
 package com.myMathTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-import junit.framework.Assert;
+public class SampleMathTest {
 
-class SampleMathTest {
+	
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+		
+
+		@Test
+		void testSum() {
+			assertEquals(10+10, 20);
+		}
+		@Test
+		void testProduct() {
+			assertEquals(10*10, 20);
+		}
 	}
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	@Test
-	void testSum() {
-		assertEquals(10+10, 20);
-	}
-	@Test
-	void testProduct() {
-		assertEquals(10*10, 20);
-	}
-}

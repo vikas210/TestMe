d59b1d3b4beb0cccc438caffcdedf8e4fc809b83
Vikas <vikas_210@yahoo.com>
2018-04-01 19:34:53 +1000

    testingNG

---
+++ ANTJunitProject/src/com/myMathTestNG/MathNewTestNG.java
@@ -0,0 +1,18 @@
+package com.myMathTestNG;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+public class MathNewTestNG {
+  
+   @Test
+  public void testPass() {
+		Assert.assertEquals(10, 5+5);
+  }
+   
+   
+   @Test
+   public void testFail() {
+ 		Assert.assertEquals(20, 5+5);
+   }
+}
